99eee70d2808eca58a7f93df661e3bd0ac3dffcb
Marius PC <mihailmariusion@gmail.com>
2024-10-25 22:08:18 +0200

    wip

--- bot/handlers/message_handler.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Respond to any text message received
-    await update.message.reply_text("Recibí tu mensaje de texto.")
-
-
